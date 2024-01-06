45ce5f717c51846d4845ed0e2454220b0221eb59
pranshubarar1 <pranshubarar1851996@gmail.com>
2024-01-07 00:42:21 +0530

    minor issues resolved

--- src/main/java/com/example/speer/utils/HelperFunctions.java
+++ src/main/java/com/example/speer/utils/HelperFunctions.java
@@ -17,6 +17,43 @@ public class HelperFunctions {
 //     * @return String
 //     */
 
+    public static String buildMultiIndexMatchBody(String query, int userId) {
+        return "{\n" +
+                "\"from\": 0,\n" +
+                "\"size\": 100,\n" +
+                "\"track_total_hits\": true,\n" +
+                "\"sort\" : {\n" +
+                "      \"noteMySqlId\": {\"order\": \"asc\"}\n" +
+                "      },\n" +
+                "  \"query\": {\n" +
+                "    \"bool\": {\n" +
+                "      \"should\": [\n" +
+                "        {\"bool\": {\n" +
+                "          \"must\": [\n" +
+                "            {\"query_string\": {\"query\": \"*" + query + "*\",\"fields\": [\"note\"], \"default_operator\": \"AND\"}},\n" +
+                "            {\"term\": {\"ownerId\": " + userId + "}}\n" +
+                "          ]\n" +
+                "        }},\n" +
+                "        {\"bool\": {\n" +
+                "          \"must\": [\n" +
+                "            {\"query_string\": {\"query\": \"*" + query + "*\",\"fields\": [\"note\"], \"default_operator\": \"AND\"}},\n" +
+                "            {\"terms\": {\"sharedWithUsers\": [" + userId + "]}}\n" +
+                "          ]\n" +
+                "        }}\n" +
+                "      ]\n" +
+                "    }\n" +
+                "  },\n" +
+                "  \"highlight\": {\n" +
+                "    \"fields\": {\n" +
+                "      \"note\": {}\n" +
+                "    },\n" +
+                "    \"require_field_match\": true\n" +
+                " }\n" +
+                "}";
+    }
+
+
+
 //    public static String buildMultiIndexMatchBody(String query, int userId) {
 //        return "{\n" +
 //                "\"from\": 0,\n" +
@@ -43,30 +80,31 @@ public class HelperFunctions {
 //                "}";
 //    }
 
-    public static String buildMultiIndexMatchBody(String query, int userId) {
-        return "{\n" +
-                "\"from\": 0,\n" +
-                "\"size\": 100,\n" +
-                "\"track_total_hits\": true,\n" +
-                "\"sort\" : {\n" +
-                "      \"noteMySqlId\": {\"order\": \"asc\"}\n" +
-                "      },\n" +
-                "  \"query\": {\n" +
-                "    \"bool\": {\n" +
-                "      \"must\": [\n" +
-                "        {\"query_string\": {\"query\": \"*" + query + "*\",\"fields\": [\"note\"], \"default_operator\": \"AND\"}},\n" +
-                "        {\"term\": {\"ownerId\": " + userId + "}}\n" +
-                "      ]\n" +
-                "    }\n" +
-                "  },\n" +
-                "  \"highlight\": {\n" +
-                "    \"fields\": {\n" +
-                "      \"note\": {}\n" +
-                "    },\n" +
-                "    \"require_field_match\": true\n" +
-                " }\n" +
-                "}";
-    }
+    //working query
+//    public static String buildMultiIndexMatchBody(String query, int userId) {
+//        return "{\n" +
+//                "\"from\": 0,\n" +
+//                "\"size\": 100,\n" +
+//                "\"track_total_hits\": true,\n" +
+//                "\"sort\" : {\n" +
+//                "      \"noteMySqlId\": {\"order\": \"asc\"}\n" +
+//                "      },\n" +
+//                "  \"query\": {\n" +
+//                "    \"bool\": {\n" +
+//                "      \"must\": [\n" +
+//                "        {\"query_string\": {\"query\": \"*" + query + "*\",\"fields\": [\"note\"], \"default_operator\": \"AND\"}},\n" +
+//                "        {\"term\": {\"ownerId\": " + userId + "}}\n" +
+//                "      ]\n" +
+//                "    }\n" +
+//                "  },\n" +
+//                "  \"highlight\": {\n" +
+//                "    \"fields\": {\n" +
+//                "      \"note\": {}\n" +
+//                "    },\n" +
+//                "    \"require_field_match\": true\n" +
+//                " }\n" +
+//                "}";
+//    }
 
 
 //    public static String buildMultiIndexMatchBody(String query, int currentuUserId) {
