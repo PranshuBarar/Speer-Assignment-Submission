fac09a82bc6c6ed49948d781bd981404aac0a343
pranshubarar1 <pranshubarar1851996@gmail.com>
2024-01-11 14:09:33 +0530

    Adding comments

--- src/main/java/com/example/speer/utils/HelperFunctions.java
+++ src/main/java/com/example/speer/utils/HelperFunctions.java
@@ -10,7 +10,71 @@ public class HelperFunctions {
 
     private static final String SEARCH_FIELD = "note";
 
-
+    //================================================================================================================
+    //================================================================================================================
+    /*
+        Now the actual query which we want for the elasticsearch looks like this:
+        {
+          "from": 0,
+          "size": 100,
+          "track_total_hits": true,
+          "sort": {
+            "id": {
+              "order": "asc"
+            }
+          },
+          "query": {
+            "bool": {
+              "should": [
+                {
+                  "bool": {
+                    "must": [
+                      {
+                        "query_string": {
+                          "query": "*{query}*",  // Replace {query} with the actual query string
+                          "fields": ["note"],
+                          "default_operator": "AND"
+                        }
+                      },
+                      {
+                        "term": {
+                          "ownerId": {userId}  // Replace {userId} with the actual user ID
+                        }
+                      }
+                    ]
+                  }
+                },
+                {
+                  "bool": {
+                    "must": [
+                      {
+                        "query_string": {
+                          "query": "*{query}*",  // Replace {query} with the actual query string
+                          "fields": ["note"],
+                          "default_operator": "AND"
+                        }
+                      },
+                      {
+                        "terms": {
+                          "sharedWithUsers": [{userId}]  // Replace {userId} with the actual user ID
+                        }
+                      }
+                    ]
+                  }
+                }
+              ]
+            }
+          },
+          "highlight": {
+            "fields": {
+              "note": {}
+            },
+            "require_field_match": true
+          }
+        }
+    */
+    //================================================================================================================
+    //================================================================================================================
     public static String buildMultiIndexMatchBody(String query, int userId) {
         return "{\n" +
                 "\"from\": 0,\n" +
